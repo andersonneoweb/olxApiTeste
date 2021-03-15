6aa544b3467223686b80c4380ec1ef223f411766
anderson gomes <andersongomesjp19@gmail.com>
2021-03-15 09:07:06 -0300

    Primeiro commit

---
+++ src/controllers/UserController.js
@@ -0,0 +1,38 @@
+const State = require('../models/State');
+const User = require('../models/User');
+const Category = require('../models/Category');
+const Ad = require('../models/Ad');
+
+module.exports = {
+    getStates: async (req, res) => {
+        let states = await State.find();
+        res.json({states});
+    },
+    info: async(req, res) => {
+        let token = req.query.token;
+
+        const user = await User.findOne({token});
+        const state = await State.findById(user.state);
+        const Ads = await Ad.find({idUser: user._id.toString()});
+
+        let adList = [];
+        for(let i in ads) {
+            const cat = await Category.findById(ads[i].category);
+            adList.push({ ...ads[i], category: cat.slug});
+            
+        }
+
+
+        res.json({
+            name: user.name,
+            email: user.email,
+            state: state.name,
+            ads: adList
+        });
+
+    },
+    editAction: async (req, res) => {
+        
+    }
+
+};
